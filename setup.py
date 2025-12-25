310cc354fb0ef27e41e77d5aadf3109485ead3aa
Arjun Krishnan <arjunkrishnan410@gmail.com>
2025-12-24 21:41:15 -0600

    updated setup to python; removed sh version

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 import os
 import stat
 import subprocess
+import sys
 from pathlib import Path
 
 
-def main(proxy_file: str, wrapper_file: str):
+def write_files(proxy_file: str, wrapper_file: str):
 	script_dir: Path = Path(__file__).resolve().parent
 
 	subprocess.run(['go', 'build', '-o', f'/usr/local/bin/{proxy_file}', script_dir.as_posix()], check=True)
-	wrapper_file = f'/usr/local/bin/${wrapper_file}'
+	wrapper_file = f'/usr/local/bin/{wrapper_file}'
 	with open(wrapper_file, 'w') as file:
 		file.write(
 			'''
@@ -28,10 +29,26 @@ def main(proxy_file: str, wrapper_file: str):
 		)
 
 	current_perms = os.stat(wrapper_file).st_mode
-	new_perms = current_perms | stat.S_IEXEC
+	new_perms = current_perms | stat.S_IXUSR | stat.S_IXOTH
 	os.chmod(wrapper_file, new_perms)
 
 	print("run 'mp' for the server and proxy engine.\nrun 'mpc' for the client connection engine.")
+
+def persistent_setup(proxy_file: str, wrapper_file: str):
+	system = "ubuntu"
+	match system:
+		case "ubuntu":
+			setup_systemctl(proxy_file, wrapper_file)
+		case _:
+			print(f'unsupported operating system: {system}')
+
+def setup_systemctl(proxy_file: str, wrapper_file: str):
+	# TODO: finish (i thought I could finish it but forgot I'm only on my mac without wifi)
+	...
+
+def main(proxy_file: str, wrapper_file: str):
+	write_files(proxy_file, wrapper_file)
+	persistent_setup(proxy_file, wrapper_file)
 
 if __name__ == '__main__':
 	proxy_file = input('what do you what to call the proxy and server engine? (default=mp) ').strip() or 'mp'
